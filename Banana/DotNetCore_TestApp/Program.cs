8128bca0b75a3a85e0a10a6e015debddb241ebf2
lio.huang <393089518@qq.com>
2018-12-07 16:50:53 +0800

    Test

--- Banana/DotNetCore_TestApp/Program.cs
+++ Banana/DotNetCore_TestApp/Program.cs
@@ -18,38 +18,18 @@ namespace DotNetCore_TestApp
 {
     class Program
     {
-        static string strConn = @"Data Source=.;Initial Catalog = AdminLTE.Net.DB;User ID=sa;Password =mimashi123";
         static void Main(string[] args)
         {
-            TestPostgres();
+            //TestSQLServer();
+
+            //TestMySQL();
+
+            //TestPostgres(); 
+
+            TestSQLite();
             Console.WriteLine("Hello World!");
             Console.ReadKey();
-        }
-
-        static async void TestAsync()
-        {
-            var repoUserInfo = new Repository<UserInfo>();
-
-            var page = await repoUserInfo.QueryAsync(1); 
-            var page1 = await repoUserInfo.QueryListAsync(1, 10, "sex=@sex", new { sex = 1 }, order: "createTime", asc: false);
-            var page2 = await repoUserInfo.QueryListAsync(2, 10, "sex=@sex", new { sex = 1 });
-            var info = await repoUserInfo.QueryListAsync("UserName=@userName and Password =@psw", new { userName = "admin", psw = "25d55ad283aa400af464c76d713c07ad" });
-
-            var count = await repoUserInfo.QueryCountAsync();
-            Show("async");
-            Show(DateTime.Now.ToString());
-            Show(page);
-            Show(DateTime.Now.ToString());
-            Show(page1.data);
-            Show(DateTime.Now.ToString());
-            Show(page2.data);
-            Show(DateTime.Now.ToString());
-            Show(info);
-            Show(DateTime.Now.ToString());
-            Show("Count："+count); 
-
-            var deleteAsync = await repoUserInfo.DeleteAsync("HeaderImg is Null", null);
-        }
+        } 
 
         #region Show
         static void Show(IEnumerable<UserInfo> infos)
@@ -90,34 +70,111 @@ namespace DotNetCore_TestApp
             return data;
         }
 
+        /// <summary>
+        /// SQLServer
+        /// </summary>
         static void TestSQLServer()
         {
-            ConnectionBuilder.ConfigRegist(strConn, Banana.Uow.Models.DBType.SqlServer);
-            var repoUserInfo = new Repository<UserInfo>(); 
+            ConnectionBuilder.ConfigRegist("Data Source=.;Initial Catalog = AdminLTE.Net.DB;User ID=sa;Password =mimashi123", DBType.SqlServer);
+            var repoUserInfo = new Repository<UserInfo>();
 
-            var model = repoUserInfo.Query(47);
-            model.Phone = "12345678";
-            repoUserInfo.Update(model); 
-            model = repoUserInfo.Query(47);
+            //repoUserInfo.Execute(@"CREATE TABLE T_User
+            //                         (
+            //                         Id INT PRIMARY KEY IDENTITY(1,1),
+            //                         UserName VARCHAR(50),
+            //                         Password VARCHAR(50),
+            //                         Name NVARCHAR(50),
+            //                         Sex INT,
+            //                         Phone VARCHAR(20),
+            //                         Enable INT,
+            //                         CreateTime DATETIME
+            //                         )", null);
+            //var datas = TestData();
+            //repoUserInfo.DBConnection.Insert(datas);
 
-            var page1 = repoUserInfo.QueryList(1, 10, "sex=@sex", new { sex = 1 }, order: "createTime", asc: false);
-            var page2 = repoUserInfo.QueryList(2, 10, "sex=@sex", new { sex = 1 });
-            var page3 = repoUserInfo.QueryList(3, 10, "sex=@sex", new { sex = 1 });
-            var page4 = repoUserInfo.QueryList(2, 5);
-            var page5 = repoUserInfo.QueryList(3, 5);
+            var list = repoUserInfo.QueryList();
 
-            var userModel = repoUserInfo.QueryList("UserName=@userName and Password =@psw", new { userName = "admin", psw = "25d55ad283aa400af464c76d713c07ad" }).FirstOrDefault();
-            //var repo = new Repository<Category>();
-            //var list = repo.QueryList("where ParentNamePath like @ParentNamePath", new { ParentNamePath = "%,电气设备,%" });
+            var page1 = repoUserInfo.QueryList(1, 5);
+            var page2 = repoUserInfo.QueryList(2, 5);
+            var page3 = repoUserInfo.QueryList(3, 5);
+
+            var model = repoUserInfo.Query(2);
+            bool b = repoUserInfo.Delete(model);
+            list = repoUserInfo.QueryList();
+
+            model = repoUserInfo.Query(3);
+            model.Phone = "1234567";
+            bool ub = repoUserInfo.Update(model);
+            list = repoUserInfo.QueryList(order: "order by id");
+            UserInfo newUser = new UserInfo()
+            {
+                Name = "eminemjk",
+                UserName = "eminemjk",
+                Phone = "12346578",
+                Enable = 1,
+                Password = "mimashi123",
+                Sex = 1,
+                CreateTime = DateTime.Now
+            };
+            int id = (int)repoUserInfo.Insert(newUser);
+        } 
+
+        /// <summary>
+        /// MySQL
+        /// </summary>
+        static void TestMySQL()
+        {
+            ConnectionBuilder.ConfigRegist("server=192.168.23.129;port=3306;database=tempdb;user=root;password=mimashi123;", DBType.MySQL);
+
+            var repoUserInfo = new Repository<UserInfo>();
+            repoUserInfo.Execute(@"CREATE TABLE IF NOT EXISTS `T_User`(
+                                       `Id` int auto_increment primary key ,
+                                       `UserName` VARCHAR(100) NOT NULL,
+                                       `Password` VARCHAR(40) NOT NULL,
+                                       `Name` VARCHAR(20),
+                                       `Sex` int,
+                                       `Phone` VARCHAR(20),
+                                       `Enable` INT,
+                                       `CreateTime` DATETIME
+                                    )  CHARSET=utf8;", null);
+            var datas = TestData();
+            repoUserInfo.DBConnection.Insert(datas);
+            var list = repoUserInfo.QueryList();
+
+            var page1 = repoUserInfo.QueryList(1, 5);
+            var page2 = repoUserInfo.QueryList(2, 5);
+            var page3 = repoUserInfo.QueryList(3, 5);
+
+            var model = repoUserInfo.Query(2);
+            bool b = repoUserInfo.Delete(model);
+            list = repoUserInfo.QueryList();
+
+            model = repoUserInfo.Query(3);
+            model.Phone = "1234567";
+            bool ub = repoUserInfo.Update(model);
+            list = repoUserInfo.QueryList(order: "order by id");
+            UserInfo newUser = new UserInfo()
+            {
+                Name = "eminemjk",
+                UserName = "eminemjk",
+                Phone = "12346578",
+                Enable = 1,
+                Password = "mimashi123",
+                Sex = 1,
+                CreateTime = DateTime.Now
+            };
+            int id = (int)repoUserInfo.Insert(newUser);
         }
 
-
+        /// <summary>
+        /// Postgres
+        /// </summary>
         static void TestPostgres()
         {
-            ConnectionBuilder.ConfigRegist("PORT=5432;DATABASE=postgres;HOST=localhost;PASSWORD=mimashi123;USER ID=postgres", Banana.Uow.Models.DBType.Postgres);
+            ConnectionBuilder.ConfigRegist("PORT=5432;DATABASE=postgres;HOST=192.168.23.129;PASSWORD=mimashi123;USER ID=postgres", DBType.Postgres);
             var repoUserInfo = new Repository<UserModel>();
-            //区分大小写，包括字段
-            //repoUserInfo.Execute( @"CREATE TABLE t_user( 
+            //区分大小写，包括字段，所以测试用另一个Model
+            //repoUserInfo.Execute(@"CREATE TABLE t_user( 
             //                            id         SERIAL      PRIMARY KEY,
             //                            username    CHAR(50)    NOT NULL,
             //                            password    CHAR(50)    NOT NULL,
@@ -125,11 +182,9 @@ namespace DotNetCore_TestApp
             //                            phone       CHAR(11),
             //                            sex int,enable int,
             //                            createtime date); ", null);
-            //var models = ModelConvertUtil<UserInfo, UserModel>.ModelCopy(TestData());
-            //foreach (var data in models)
-            //{
-            //    repoUserInfo.Insert(data);
-            //}
+            //var datas = ModelConvertUtil<UserInfo, UserModel>.ModelCopy(TestData());
+            //repoUserInfo.DBConnection.Insert(datas);
+
             var list = repoUserInfo.QueryList();
 
             var page1 = repoUserInfo.QueryList(1, 5);
@@ -143,10 +198,63 @@ namespace DotNetCore_TestApp
             model = repoUserInfo.Query(3);
             model.phone = "1234567";
             bool ub = repoUserInfo.Update(model);
-            list = repoUserInfo.QueryList(order:"order by id");
+            list = repoUserInfo.QueryList(order: "order by id");
         }
 
+        /// <summary>
+        /// SQLite
+        /// </summary>
+        static void TestSQLite()
+        {
+            //System.Data.SQLite.SQLiteConnection.CreateFile("SqliteDemo");
+            ConnectionBuilder.ConfigRegist("Data Source=SqliteDemo;Version=3;", DBType.SQLite);
 
+            var repoUserInfo = new Repository<UserInfo>();
+
+            //repoUserInfo.Execute(@"CREATE TABLE T_User
+            //                         (
+            //                         Id Integer Primary Key Autoincrement,
+            //                         UserName VARCHAR(50),
+            //                         Password VARCHAR(50),
+            //                         Name NVARCHAR(50),
+            //                         Sex Integer,
+            //                         Phone VARCHAR(20),
+            //                         Enable Integer,
+            //                         CreateTime DATETIME
+            //                         )", null);
+            //var datas = TestData();
+            //repoUserInfo.DBConnection.Insert(datas);
+             
+            var list = repoUserInfo.QueryList();
+
+            var page1 = repoUserInfo.QueryList(1, 5);
+            var page2 = repoUserInfo.QueryList(2, 5);
+            var page3 = repoUserInfo.QueryList(3, 5);
+
+            var model = repoUserInfo.Query(2);
+            bool b = repoUserInfo.Delete(model);
+            list = repoUserInfo.QueryList();
+
+            model = repoUserInfo.Query(3);
+            model.Phone = "1234567";
+            bool ub = repoUserInfo.Update(model);
+            list = repoUserInfo.QueryList(order: "order by id");
+            UserInfo newUser = new UserInfo()
+            {
+                Name = "eminemjk",
+                UserName = "eminemjk",
+                Phone = "12346578",
+                Enable = 1,
+                Password = "mimashi123",
+                Sex = 1,
+                CreateTime = DateTime.Now
+            };
+            int id = (int)repoUserInfo.Insert(newUser);
+        }
+
+        /// <summary>
+        /// UnitOfWork
+        /// </summary>
         static void TestUow()
         {
             //ConnectionBuilder.ConfigRegist(strConn, Banana.Uow.Models.DBType.SqlServer);
@@ -167,6 +275,34 @@ namespace DotNetCore_TestApp
             //        uow.Rollback();
             //    }
             //}
+        }
+
+        /// <summary>
+        /// Async
+        /// </summary>
+        static async void TestAsync()
+        {
+            var repoUserInfo = new Repository<UserInfo>();
+
+            var page = await repoUserInfo.QueryAsync(1);
+            var page1 = await repoUserInfo.QueryListAsync(1, 10, "sex=@sex", new { sex = 1 }, order: "createTime", asc: false);
+            var page2 = await repoUserInfo.QueryListAsync(2, 10, "sex=@sex", new { sex = 1 });
+            var info = await repoUserInfo.QueryListAsync("UserName=@userName and Password =@psw", new { userName = "admin", psw = "25d55ad283aa400af464c76d713c07ad" });
+
+            var count = await repoUserInfo.QueryCountAsync();
+            Show("async");
+            Show(DateTime.Now.ToString());
+            Show(page);
+            Show(DateTime.Now.ToString());
+            Show(page1.data);
+            Show(DateTime.Now.ToString());
+            Show(page2.data);
+            Show(DateTime.Now.ToString());
+            Show(info);
+            Show(DateTime.Now.ToString());
+            Show("Count：" + count);
+
+            var deleteAsync = await repoUserInfo.DeleteAsync("HeaderImg is Null", null);
         }
     }
 }
